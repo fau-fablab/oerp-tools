e0fc74de154b9d5e47816bfc0d413f2a643a2e92
Patrick Kanzler <dev@pkanzler.de>
2017-08-18 15:57:57 +0200

    add stub with argcomplete

--- update-oerp-product-amount.py
+++ update-oerp-product-amount.py
@@ -8,7 +8,7 @@ snapshot of our cash register.
 :author: `Patrick Kanzler <patrick.kanzler@fablab.fau.de>`_
 :organization: `FAU FabLab <https://fablab.fau.de>`_
 :copyright: Copyright (c) 2017 FAU FabLab
-:license: MIT
+:license: GPLv3
 """
 
 from __future__ import absolute_import
@@ -16,3 +16,49 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import logging
+import argparse
+import sys
+
+__authors__ = "Patrick Kanzler <patrick.kanzler@fablab.fau.de>"
+__license__ = "GPLv3"
+__copyright__ = "Copyright (C) 2017 {}".format(__authors__)
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+logger.debug('initializing logger')
+
+try:
+    from argcomplete import autocomplete
+except ImportError:
+    def autocomplete_wrapper(*args):
+        print_error("Consider installing argcomplete")
+    autocomplete = autocomplete_wrapper
+
+
+def print_error(message):
+    """ prints an error message to stderr
+    """
+    logger.error(message)
+    print("[!] {}".format(message), file=sys.stderr)
+
+
+def parse_args():
+    """
+    Parses the command line arguments and returns them in an object
+    """
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    autocomplete(parser)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+
+if __name__ == "__main__":
+    main()
