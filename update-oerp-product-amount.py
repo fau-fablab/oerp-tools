b57e0792338aab42f6609b6caa78d93671664b0c
Patrick Kanzler <dev@pkanzler.de>
2017-08-18 15:17:17 +0200

    add stump for oerp-update-script

---
+++ update-oerp-product-amount.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+""" Update OERP product amount
+
+This script updates the product quantities in OERP based on a database
+snapshot of our cash register.
+
+:author: `Patrick Kanzler <patrick.kanzler@fablab.fau.de>`_
+:organization: `FAU FabLab <https://fablab.fau.de>`_
+:copyright: Copyright (c) 2017 FAU FabLab
+:license: MIT
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
